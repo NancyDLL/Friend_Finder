bf67e23d3ee79b55997314bd3746357212038d4f
NancyDLL <nano14@mac.com>
2017-10-04 13:16:21 -0500

    made changes to routes

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -2,7 +2,8 @@
 // LOAD DATA - From Hot Restraurant Example
 // Linking routes to the friend "data" source.
 // ===============================================================================
-var friends = require("/app/data/friends");
+var path = require('path');
+var friends = require("../data/friends.js");
 
 
 // ===============================================================================
@@ -11,7 +12,7 @@ var friends = require("/app/data/friends");
 module.exports = function(app) {
   // API GET Request
   // ---------------------------------------------------------------------------
-  app.get("../app/data/friends", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
@@ -24,6 +25,32 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("../app/data/friends", function(req, res) {
+  app.post("/api/friends", function(req, res) {
   // will handle the incoming survey results and compatibility logic
-  });
+   var userInput = req.body;
+      var userResponse = userInput.scores;
+      var match = {
+        name: "",
+        photo: "",
+        difference: 1000
+      };
+
+      for (var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
+        for (var x = 0; x < userResponse.length; x++) {
+          totalDifference += Math.abs(friends[i].scores[x] - userResponse[x]);
+          
+          if (totalDifference <= match.difference){
+              match.name = friends[i].name;
+              match.photo = friends[i].photo;
+              match.difference = totalDifference;
+          }
+        }
+      }
+
+      friends.push(userInput);
+
+      res.json(match);
+
+    });
+  };
